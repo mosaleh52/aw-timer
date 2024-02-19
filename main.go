5933f1d5663a064106b181df0ee4ebf135d580cd
Mohamed <mosaleh@mail.com>
2024-02-19 12:43:09 +0200

    implement stop command and add it to main with sperate file

--- main.go
+++ main.go
@@ -1,26 +1,12 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 )
 
 func main() {
 	rootCmd := &cobra.Command{Use: "todoTimer"}
 
-	cmdStop := &cobra.Command{
-		Use:   "stop",
-		Short: "stop current running todo",
-		Run: func(cmd *cobra.Command, args []string) {
-			if len(args) == 0 {
-				// get the current todo and stop it by adding duriont to aw-entry  returning the stoped todo
-			} else {
-				fmt.Println("Error: the stop command does not accept any commands")
-			}
-		},
-	}
-
-	rootCmd.AddCommand(startTimerCmd(), cmdStop, getCurrentTodoCmd())
+	rootCmd.AddCommand(startTimerCmd(), stopTimerCmd(), getCurrentTodoCmd())
 	rootCmd.Execute()
 }
---
+++ stopTimerCmd.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func stopTimerCmd() *cobra.Command {
+	var apiUrl, bucketId, dateLayout string
+	cmd := &cobra.Command{
+		Use:   "stop",
+		Short: "stop a task",
+		Run: func(cmd *cobra.Command, args []string) {
+			if len(args) == 0 {
+				stopTimer(apiUrl, bucketId, dateLayout, getCurrentTodo(apiUrl, bucketId, dateLayout)[0].ID)
+			} else {
+				fmt.Println("Error: you can only stop timer for one todo")
+			}
+		},
+	}
+
+	cmd.Flags().StringVarP(&apiUrl, "api-url", "u", "http://127.0.0.1:5600/api/0/", "specify the api url ")
+	cmd.Flags().StringVarP(&bucketId, "bucket-id", "b", "aw-stopwatch", "specify the bucket-id")
+	cmd.Flags().StringVarP(&dateLayout, "date-Layout", "d", "2006-01-02T15:04:05.999999-07:00", "specify the dateLayout used for formatting in aw server")
+	return cmd
+}
